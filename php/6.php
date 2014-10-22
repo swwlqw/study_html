c0dd9327377c270fd1f0e5190458b37b917336a8
释一 <quanwei.lqw@alibaba-inc.com>
2014-10-22 14:24:41 +0800

    modify 6.php

--- php/6.php
+++ php/6.php
@@ -3,8 +3,8 @@ function sum($x,$y){
 	$z=$x+$y;
 	return $z;
 }
-echo "5 + 7 = " . sum(5,7) . "<br/>"
-echo "1 + 7 = " . sum(1,7) . "<br/>"
-echo "5 + 9 = " . sum(5,9) . "<br/>"
-echo "11 + 7 = " . sum(11,7) . "<br/>"
+echo "5 + 7 = " . sum(5,7) . "<br/>";
+echo "1 + 7 = " . sum(1,7) . "<br/>";
+echo "5 + 9 = " . sum(5,9) . "<br/>";
+echo "11 + 7 = " . sum(11,7) . "<br/>";
 ?>
